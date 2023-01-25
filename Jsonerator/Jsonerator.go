f947041419d7b47949cd7574113af5f2dc352cf0
john <johnrin1987@gmail.com>
2023-01-24 20:19:54 -0600

    updating parser for arrays

--- Jsonerator/Jsonerator.go
+++ Jsonerator/Jsonerator.go
@@ -4,12 +4,8 @@
 
 package Jsonerator
 
-import (
-	"fmt"
-	"unicode"
-)
-
 func (l *Lexer) peekChar(data []rune, posit int) string {
+	//fmt.Println(string(data[posit+1]))
 	return string(data[posit+1])
 }
 
@@ -17,6 +13,120 @@ func (l *Lexer) peekRune(data []rune, posit int) rune {
 	return data[posit+1]
 }
 
+func (l *Lexer) ParseArray(data []rune, posit int, tks *Tokens) {
+
+}
+
+func (l *Lexer) BuildValues(data []rune, posit int, tk *Token) {
+	var str_holder string
+	size := len(data)
+
+	//iterate through current position in our data
+	//data = "{\"$implementationId\":\"deviceConfiguration--hardenedUncPathEnabled\",\"hardenedUncPaths\":[{\"serverPath\":\"\\\\\\\\*\\\\SYSVOL\",\"securityFlags\":[\"requireMutualAuthentication\",\"requireIntegrity\"]},{\"serverPath\":\"\\\\\\\\*\\\\NETLOGON\",\"securityFlags\":[\"requireMutualAuthentication\",\"requireIntegrity\"]}]}"
+
+	for i := posit; i < size; i++ {
+		//store the character in string
+		str_holder = string((data[i]))
+		//this indicates the start of a key
+		if str_holder == "{" && l.peekChar(data, (i+1)) == "\"" {
+
+		}
+
+	}
+}
+
+func (l *Lexer) BuildKey(data []rune, posit int, tk *Token) {
+	var str_holder string
+	var key_holder string
+	counter := posit
+	size := len(data)
+
+	//iterate through current position in our data
+	//data = "{\"$implementationId\":\"deviceConfiguration--hardenedUncPathEnabled\",\"hardenedUncPaths\":[{\"serverPath\":\"\\\\\\\\*\\\\SYSVOL\",\"securityFlags\":[\"requireMutualAuthentication\",\"requireIntegrity\"]},{\"serverPath\":\"\\\\\\\\*\\\\NETLOGON\",\"securityFlags\":[\"requireMutualAuthentication\",\"requireIntegrity\"]}]}"
+
+	for i := posit; i < size; i++ {
+		//store the character in string
+		str_holder = string((data[i]))
+		//this indicates the start of a key
+		if str_holder != ":" {
+			key_holder += str_holder
+		} else if str_holder == ":" {
+			break
+		}
+		counter++
+	}
+	tk.Key = key_holder
+	l.Posit = counter
+}
+
+func (l *Lexer) BuildToken(data []rune, posit int, tk *Token) {
+	var str_holder string
+	size := len(data)
+
+	//iterate through current position in our data
+	//data = "{\"$implementationId\":\"deviceConfiguration--hardenedUncPathEnabled\",\"hardenedUncPaths\":[{\"serverPath\":\"\\\\\\\\*\\\\SYSVOL\",\"securityFlags\":[\"requireMutualAuthentication\",\"requireIntegrity\"]},{\"serverPath\":\"\\\\\\\\*\\\\NETLOGON\",\"securityFlags\":[\"requireMutualAuthentication\",\"requireIntegrity\"]}]}"
+
+	for i := posit; i < size; i++ {
+		//store the character in string
+		str_holder = string((data[i]))
+		//this indicates the start of a key
+		if str_holder == "\"" && string(data[i-1]) == "{" {
+			l.BuildKey(data, i, tk)
+			i = l.Posit
+			//this indicates the start of a simple KEY
+		} else if string(data[i-1]) == ":" && str_holder == "\"" {
+			l.BuildValues(data, i, tk)
+			i = l.Posit
+		}
+	}
+
+}
+
+func (l *Lexer) ParseJson(data []rune, posit int, tks *Tokens) {
+	//this will be the current token we're working with
+	var tok_holder Token
+	var str_holder string
+	size := len(data)
+
+	//iterate through current position in our data
+	//data = "{\"$implementationId\":\"deviceConfiguration--hardenedUncPathEnabled\",\"hardenedUncPaths\":[{\"serverPath\":\"\\\\\\\\*\\\\SYSVOL\",\"securityFlags\":[\"requireMutualAuthentication\",\"requireIntegrity\"]},{\"serverPath\":\"\\\\\\\\*\\\\NETLOGON\",\"securityFlags\":[\"requireMutualAuthentication\",\"requireIntegrity\"]}]}"
+
+	for i := posit; i < size; i++ {
+		//store the character in string
+		str_holder = string((data[i]))
+		//this indicates the start of a key
+		if str_holder == "{" && l.peekChar(data, (i)) == "\"" {
+			l.BuildToken(data, (i + 1), &tok_holder)
+		} else if str_holder == ":" && l.peekChar(data, (i+1)) == "[" {
+			l.ParseArray(data, i, tks)
+		}
+
+	}
+
+}
+
+func GetKeyVals(data string) Tokens {
+	var toks Tokens
+	/*
+		var tok Token
+
+	*/
+	var lex Lexer
+
+	chars := []rune(data)
+
+	size := len(chars)
+
+	//reserved_chars := [7]string {"{","}","[","]","\"", ",",":"}
+	//data = "{\"$implementationId\":\"deviceConfiguration--hardenedUncPathEnabled\",\"hardenedUncPaths\":[{\"serverPath\":\"\\\\\\\\*\\\\SYSVOL\",\"securityFlags\":[\"requireMutualAuthentication\",\"requireIntegrity\"]},{\"serverPath\":\"\\\\\\\\*\\\\NETLOGON\",\"securityFlags\":[\"requireMutualAuthentication\",\"requireIntegrity\"]}]}"
+	for i := 0; i < size; i++ {
+		lex.ParseJson(chars, i, &toks)
+	}
+
+	return toks
+}
+
+/*
 func (l *Lexer) parseKeys(data []rune, posit int, t *Token) {
 	var holder string
 	size := len(data)
@@ -127,3 +237,5 @@ func GetKeyVals(data string) Tokens {
 
 	return toks
 }
+
+*/
